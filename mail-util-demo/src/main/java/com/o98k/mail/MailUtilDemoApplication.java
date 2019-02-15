ce95371bbb289d5994a5dc42b81103f86bcd5d0e
DavidZh0u <zhoudawei001@huazhu.com>
2019-02-15 14:02:06 +0800

    new

---
+++ mail-util-demo/src/main/java/com/o98k/mail/MailUtilDemoApplication.java
@@ -0,0 +1,14 @@
+package com.o98k.mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailUtilDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailUtilDemoApplication.class, args);
+    }
+
+}
+
